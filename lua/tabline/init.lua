0f225707fa57972c691731ba0cb9cefff13d63b6
KicamonIce <1532931259@qq.com>
2023-11-20 11:03:20 +0800

    coro

--- lua/tabline/init.lua
+++ lua/tabline/init.lua
@@ -13,58 +13,53 @@ local function getinfo(bufnr)
   }
 end
 
-Simple_Tab.devicon = function(bufnr, isSelected)
+local function devicon(bufnr, isSelected)
   local icon, devhl
   local info = getinfo(bufnr)
-  local buftype, filetype = info.buftype, info.filetype
   local ok, devicons = pcall(require, 'nvim-web-devicons')
   if not ok then
     return ''
   end
-  if buftype == 'terminal' then
+  if info.buftype == 'terminal' then
     icon, devhl = devicons.get_icon_color_by_filetype('zsh', { default = true })
   else
-    icon, devhl = devicons.get_icon_color_by_filetype(filetype, { default = true })
+    icon, devhl = devicons.get_icon_color_by_filetype(info.filetype, { default = true })
   end
   if icon then
     local attr = {
       fg = devhl,
     }
-    local hl = tbl_hl(filetype, attr)
+    local hl = tbl_hl(info.filetype, attr)
     local selectedHlStart = (isSelected and hl) and '%#' .. hl .. '#' or ''
     return selectedHlStart .. icon .. ' '
   end
   return ''
 end
 
-Simple_Tab.title = function(bufnr)
+local function title(bufnr)
   local name
   local info = getinfo(bufnr)
-  local file, buftype, filetype = info.file, info.buftype, info.filetype
 
-  if buftype == 'help' then
-    name = 'help:' .. vim.fn.fnamemodify(file, ':t:r')
-  elseif filetype == 'TelescopePrompt' then
+  if info.buftype == 'help' then
+    name = 'help:' .. vim.fn.fnamemodify(info.file, ':t:r')
+  elseif info.filetype == 'TelescopePrompt' then
     name = 'Telescope'
-  elseif buftype == 'terminal' then
-    local _, mtch = string.match(file, "term:(.*):(%a+)")
-    name = mtch ~= nil and mtch or vim.fn.fnamemodify(vim.env.SHELL, ':t')
-  elseif file == '' then
+  elseif info.file == '' then
     name = '[No Name]'
   else
-    name = vim.fn.pathshorten(vim.fn.fnamemodify(file, ':p:~:t'))
+    name = vim.fn.pathshorten(vim.fn.fnamemodify(info.file, ':p:~:t'))
   end
   return '%#TabLineSel#' .. name
 end
 
-Simple_Tab.modified = function(bufnr)
+local function modified(bufnr)
   local modicon = vim.fn.getbufvar(bufnr, '&modified') == 1 and '●' or ''
   local hl = tbl_hl("modified", { fg = "#ff461f" })
   modicon = '%#' .. hl .. '#' .. modicon
   return modicon
 end
 
-Simple_Tab.cell = function(index)
+local function cell(index)
   local isSelected = vim.fn.tabpagenr() == index
   local buflist = vim.fn.tabpagebuflist(index)
   local winnr = vim.fn.tabpagewinnr(index)
@@ -72,26 +67,32 @@ Simple_Tab.cell = function(index)
   local hl = (isSelected and '%#TabLineSel#' or '%#TabLine#')
 
   return hl .. '%' .. index .. 'T' .. ' ' ..
-      Simple_Tab.devicon(bufnr, isSelected) ..
-      Simple_Tab.title(bufnr) .. ' ' ..
-      Simple_Tab.modified(bufnr) .. '%T'
+      devicon(bufnr, isSelected) ..
+      title(bufnr) .. ' ' ..
+      modified(bufnr) .. '%T'
 end
 
-Simple_Tab.tabline = function()
-  local cell = Simple_Tab.cell
-  local line = ''
-  for i = 1, vim.fn.tabpagenr('$'), 1 do
-    line = line .. cell(i)
+local tbl_rendera = coroutine.create(function()
+  while true do
+    local line = ''
+    for i = 1, vim.fn.tabpagenr('$'), 1 do
+      line = line .. cell(i)
+    end
+    line = line .. '%#TabLineFill#%='
+    vim.opt.tabline = line
+    coroutine.yield()
   end
-  line = line .. '%#TabLineFill#%='
-  return line
+end)
+
+function Simple_Tab.setup()
+  local events = { 'BufEnter', 'BufWritePost', 'BufModifiedSet', 'TabNew', 'TabEnter', 'TabLeave' }
+  vim.api.nvim_create_autocmd(events, {
+    callback = function()
+      vim.schedule(function()
+        coroutine.resume(tbl_rendera)
+      end)
+    end
+  })
 end
 
-local setup = function()
-  vim.opt.tabline = "%!v:lua.require'tabline'.tabline.tabline()"
-end
-
-return {
-  tabline = Simple_Tab,
-  setup = setup,
-}
+return Simple_Tab
